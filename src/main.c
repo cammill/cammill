f302740b325fa914021c6b80a79024a8396455d0
Oliver Dippel <o.dippel@gmx.de>
2015-06-25 02:14:21 +0200

    fix n++

--- src/main.c
+++ src/main.c
@@ -2098,8 +2098,8 @@ void load_files () {
 					gtk_list_store_insert_with_values(ListStore[P_H_POST], NULL, -1, 0, NULL, 1, pname, -1);
 				}
 				strncpy(postcam_plugins[n], pname, sizeof(postcam_plugins[n]));
+				n++;
 				postcam_plugins[n][0] = 0;
-                                n++;
 				free(pname);
 				if (PARAMETER[P_H_POST].vint == -1) {
 					PARAMETER[P_H_POST].vint = 0;
